a5fea5b8eaba2d0264f7ba47f9d925e6df916fca
Giovanni Pini <giovanni.pini@fleetmatics.com>
2019-03-29 17:21:15 +0100

    Modifying test component for demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 import { PanelService } from 'projects/ngx-panels/src/lib';
 import { TestComponent } from './test/test.component';
 
+
+export class Robot {
+    name: string;
+    avatar: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -14,7 +20,11 @@ export class AppComponent {
     ) { }
 
     open() {
-        this.panelService.openAsRoot(TestComponent);
+        this.panelService.openAsRoot(TestComponent, {
+            name: 'Giovanni Pini',
+            avatar: 'https://robohash.org/giovannipini',
+            power: 4
+        });
     }
 
 }
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PanelRef } from 'projects/ngx-panels/src/lib';
 
 @Component({
@@ -6,14 +6,31 @@ import { PanelRef } from 'projects/ngx-panels/src/lib';
     templateUrl: './test.component.html',
     styleUrls: ['./test.component.scss']
 })
-export class TestComponent {
+export class TestComponent implements OnInit {
 
-  constructor(
-      private readonly panelRef: PanelRef<any>
-  ) { }
+    name: string;
 
-  onClick() {
-      this.panelRef.close();
-  }
+    avatar: string;
+    statement: string;
+    power: number;
+
+    constructor(
+        private readonly panelRef: PanelRef<any>
+    ) { }
+
+    ngOnInit() {
+        this.name = this.panelRef.data.name;
+        this.avatar = this.panelRef.data.avatar;
+        this.power = this.panelRef.data.power;
+        this.statement = `Hi! I'm ${this.name}. How can I help you?`;
+    }
+
+    onClick() {
+        this.panelRef.close();
+    }
+
+    onSpeak() {
+        alert(this.statement);
+    }
 
 }
