c4b44a8800967c9bedc56ebfee8ff857a93cf14c
Nicola Tommasi <nicola.tommasi@fleetmatics.com>
2019-03-29 17:22:26 +0100

    Adding robots

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { PanelService } from 'projects/ngx-panels/src/lib';
 
+import { Hero } from './models/hero.model';
 import { TestComponent } from './test/test.component';
 
 @Component({
@@ -9,13 +10,17 @@ import { TestComponent } from './test/test.component';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  robots: Hero[] = [
+    { id: 1, name: 'Ufo Robot', avatar: '', power: 1, statement: '' },
+    { id: 2, name: 'Zoran Power', avatar: '', power: 1, statement: '' },
+    { id: 3, name: 'Zin Jen', avatar: '', power: 1, statement: '' },
+    { id: 4, name: 'Mortadella', avatar: '', power: 1, statement: '' },
+    { id: 5, name: 'Paolo Bot', avatar: '', power: 1, statement: '' }
+  ];
 
-    constructor(
-        private readonly panelService: PanelService
-    ) { }
+  constructor(private readonly panelService: PanelService) {}
 
-    open() {
-        this.panelService.openAsRoot(TestComponent);
-    }
-
+  open() {
+    this.panelService.openAsRoot(TestComponent);
+  }
 }
---
+++ src/app/models/hero.model.ts
@@ -0,0 +1,7 @@
+export class Hero {
+    id: number;
+  name: string;
+  avatar: string;
+  statement: string;
+  power: number;
+}
