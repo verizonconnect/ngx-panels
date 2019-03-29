8fce0235447a785733cdcc0ff258374947b47fe3
Nicola Tommasi <nicola.tommasi@fleetmatics.com>
2019-03-29 17:10:31 +0100

    Adding style to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { PanelService } from 'projects/ngx-panels/src/lib';
+
 import { TestComponent } from './test/test.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
